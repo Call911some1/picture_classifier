4939b768ed83a96be7e8003e1a155e17c439e0df
Call911some1 <andtaras@yahoo.com>
2024-10-11 17:00:18 +0400

    updated code

--- app.py
+++ app.py
@@ -37,11 +37,11 @@ def load_model(model_name):
     if model_name == "ResNet-50":
         model = models.resnet50()
         model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
-        model.load_state_dict(torch.load('/home/andrey/Downloads/picture_classifier/models/resnet50_eurosat.pth'))
+        model.load_state_dict(torch.load('models/resnet50_eurosat.pth'))  # Используем относительный путь
     elif model_name == "EfficientNet-B0":
         model = efficientnet_b0(weights=None)
         model.classifier[1] = torch.nn.Linear(model.classifier[1].in_features, num_classes)
-        model.load_state_dict(torch.load('/home/andrey/Downloads/picture_classifier/models/efficientnet_b0_eurosat.pth'))
+        model.load_state_dict(torch.load('models/efficientnet_b0_eurosat.pth'))  # Используем относительный путь
     model.eval()
     return model
 
